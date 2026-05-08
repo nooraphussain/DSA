3a2ad545c85a00c953e3bf73648e10f130f42852
nooraphussain <noorahussainp2002@gmail.com>
2026-05-08 19:56:59 +0530

    theories should know

---
+++ hashTable.js
@@ -0,0 +1,13 @@
+// hash table
+// hanshing
+// hash function
+// collision
+// solutions : 
+//  1. Chaining (open hashing)
+//  2. Open Addressing (closed hashing)
+//    - Linear probing
+//    - Quadratic probing
+//    - Double hashing
+
+// Load factor
+// Rehashing - what, when
