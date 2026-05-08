28642cdf862559a0345eef846f73215f77d0c578
nooraphussain <noorahussainp2002@gmail.com>
2026-05-08 19:01:19 +0530

    Spanning tree theory
    what, MST, qstns

---
+++ spanningTree.js
@@ -0,0 +1,52 @@
+// A spanning tree is a concept from graph theory.
+
+// A subgraph that includes all vertices (nodes) of the original graph
+// while forming a tree (no cycles).
+
+// A connected subgraph is said to be spanning tree if and only if :
+//  it has all the vertices of the graph
+//  v-1 edges connected in acyclic manner
+
+//  A spanning tree:
+
+// ✅ Includes all vertices
+// ✅ Is connected
+// ✅ Has no cycles
+// ✅ Contains exactly:
+
+// n−1
+
+// edges for n vertices.
+
+
+// What is MST?
+// Minimum Spanning Tree (MST)
+
+// If edges have weights:
+// (weight/cost/distance)
+
+// Then:
+// Minimum Spanning Tree is the spanning tree
+// with minimum total edge weight.
+
+
+
+// Q1. Can a disconnected graph have a spanning tree?
+
+// ❌ No.
+
+// Because spanning tree must connect all vertices.
+
+// Q2. How many edges in spanning tree?
+
+// For n vertices:
+
+// n−1
+
+// Q3. Can a graph have multiple spanning trees?
+
+// ✅ Yes.
+
+// Q4. Does spanning tree contain cycles?
+
+// ❌ Never.
